51a745b76feccc07a8dfb11153e3e48a32a00269
thieuquang18 <thieuquang01082006@gmail.com>
2026-03-17 15:32:14 +0700

    Update defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -140,7 +140,7 @@ void            argaddr(int, uint64 *);
 int             fetchstr(uint64, char*, int);
 int             fetchaddr(uint64, uint64*);
 void            syscall();
-
+uint64 sys_trace(void);
 // trap.c
 extern uint     ticks;
 void            trapinit(void);
